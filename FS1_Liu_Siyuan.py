b4fab39dc91851702c5bd147e5589e1e796fcc15
koallen <weifengzi2009@gmail.com>
2014-11-09 15:14:25 +0800

    convert words into lowercase

--- FS1_Liu_Siyuan.py
+++ FS1_Liu_Siyuan.py
@@ -18,11 +18,14 @@ def read_into_list():
 def remove_characters(word_list):
     """This function remove all non-alphabetical characters of the given word list"""
     for word in word_list:
+        word = word.lower()
         word = "".join(char for char in word if char.isalpha())
     return word_list
 
 word_list = read_into_list()
 alpha_word_list = remove_characters(word_list)
+unique_words = set(alpha_word_list)
+print(len(unique_words))
 
 #calculate time used to run the python script
 net_Time = time() - start_Time
