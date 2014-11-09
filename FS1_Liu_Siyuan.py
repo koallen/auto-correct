f34c4dd2d67aac6d3ff3793ca47fe7fae28845ab
koallen <weifengzi2009@gmail.com>
2014-11-09 14:38:52 +0800

    construct read_into_list()

--- FS1_Liu_Siyuan.py
+++ FS1_Liu_Siyuan.py
@@ -5,11 +5,16 @@ start_Time = time()
 def read_into_list():
     """This function reads the file 'big.txt' into a list."""
     everyword = [] #create a empty list to save everyword in the file
+    number_of_words = 0
     with open("big.txt") as wordbase:
         lines = wordbase.readlines()
         for line in lines:
             words = line.split()
             everyword += words
+            number_of_words += len(words)
+    print("There are a total of {:d}".format(number_of_words))
+
+read_into_list()
 
 #calculate time used to run the python script
 net_Time = time() - start_Time
