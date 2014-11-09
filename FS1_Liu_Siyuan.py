5afb7ea3445d36735ee6181908d3491eaad79214
koallen <weifengzi2009@gmail.com>
2014-11-09 14:33:04 +0800

    added function read_into_list()

--- FS1_Liu_Siyuan.py
+++ FS1_Liu_Siyuan.py
@@ -2,6 +2,15 @@ from time import time
 
 start_Time = time()
 
-net_Time = time() - start_Time
+def read_into_list():
+    """This function reads the file 'big.txt' into a list."""
+    everyword = [] #create a empty list to save everyword in the file
+    with open("big.txt") as wordbase:
+        lines = wordbase.readlines()
+        for line in lines:
+            words = line.split()
+            everyword += words
 
+#calculate time used to run the python script
+net_Time = time() - start_Time
 print("It took {:0.3f} sec".format(net_Time))
